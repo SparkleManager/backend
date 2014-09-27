913524bdb9d057389baf4f2d5403d7056069cc94
DutoFujamee <dmorize@hotmail.fr>
2014-09-27 17:59:39 +0200

    Add name fonction

--- models/model.php
+++ models/model.php
@@ -1,3 +1,28 @@
 <?php
-	// AppleJAck is best pony
+class Table
+{
+	public function __construct($table)
+	{
+	}
+	
+	public function insert(array $line)
+	{
+
+	}
+	
+	public function get(array $where, array $fields = 'all')
+	{
+		
+	}
+	
+	public function update(array $where, array $line)
+	{
+
+	}
+	
+	public function delete(array $where)
+	{
+	
+	}
+}
 ?>

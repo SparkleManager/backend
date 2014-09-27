32306f269c658b04cdc68e45aea4a8270065f4dc
DutoFujamee <dmorize@hotmail.fr>
2014-09-27 18:03:11 +0200

    Add content for insert & update

--- models/model.php
+++ models/model.php
@@ -1,13 +1,30 @@
 <?php
 class Table
 {
+
+	protected $host = 'localhost';
+	protected $port = '';
+	protected $bdd = 'bronydays';
+	protected $user = 'root';
+	protected $password = '';
+	protected $pdo = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$bdd, $user, $password);
+	
+	protected $table = "";
+
 	public function __construct($table)
 	{
+		$this->table = $table;
 	}
 	
 	public function insert(array $line)
 	{
+		$tmp = separateArrayLine($line);
+		$field = $tmp[0];
+		$value = $tmp[1];
 
+		$result = "INSERT INTO `".$this->table."` (".$field.") VALUES (".$value.")";
+
+		return;
 	}
 	
 	public function get(array $where, array $fields = 'all')
@@ -17,12 +34,31 @@ class Table
 	
 	public function update(array $where, array $line)
 	{
-
+		$tmp = separateArrayLine($line);
+		$field = $tmp[0];
+		$value = $tmp[1];
+	
+		$result = "UPDATE  `".$this->table."` SET (".$value.")";
+		
+		return;
 	}
 	
 	public function delete(array $where)
 	{
 	
+	}
+	
+	protected separateArrayLine(array $line)
+	{
+		foreach ($line as $onefield => $onevalue)
+		{
+			$field = $field.$onefield.",";
+			$value = $value."'".$onevalue."',";
+		}
+		$field = substr($field,0,-1);
+		$value = substr($field,0,-1);
+		
+		return [$field,$value];
 	}
 }
 ?>

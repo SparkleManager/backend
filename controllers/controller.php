0804c4e34b531f1a5c815ac06071d1268210080d
HellOfBSOD <floflo006111993>
2014-09-29 13:11:20 +0200

    Added class controller

---
+++ controllers/controller.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shujaa
+ * Date: 29/09/2014
+ * Time: 13:06
+ */
+
+abstract class Controller {
+    public static $isAccessible = true;
+    public static $needsLogin = true;
+} 

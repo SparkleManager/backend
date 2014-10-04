ed17b156aca6595590a9d47a0a0dfbeb2111d582
HellOfBSOD <floriancramoisan@gmail.com>
2014-10-04 19:50:55 +0200

    Added sparkleLogger and LogModel, integrating changes to config and errorCodes

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-// TODO Ceci est un exemple de format, Seris avec son api.php devra définir son standard (en langage PHP !!!)
-$config['sessionTTL'] = '3600'; // Session expire delay in seconds
-
-?>
+$this->config = array(
+    "debug" => true,
+    "sessionTTL" => 3600,
+);

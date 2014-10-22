5abb3e9dc68ba1bd4d2cc517daacfa6da1d07f65
Hell_of_BSOD <florian.cramoisan@gmail.com>
2014-10-22 18:13:14 +0200

    Added mysql params

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,4 +3,12 @@
 $this->config = array(
     "debug" => true,
     "sessionTTL" => 3600,
+    
+    "mysqlHost" => "localhost",
+    "mysqlPort" => 3306,
+    "mysqlUser" => "sparklemanager",
+    "mysqlPass" => "shining",
+    "mysqlDb" => "sparklemanager",
+    "mysqlCharset" => "utf8",
+    
 );
